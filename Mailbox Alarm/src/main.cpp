1395e135f257273db7c78502813612cb0abf8d08
robmatania <rmatania@gmail.com>
2021-05-16 22:33:12 +0200

    new project

---
+++ Mailbox Alarm/src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  #define ONBOARD_LED  23
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
