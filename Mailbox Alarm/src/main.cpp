d935c4ba6a290d72ae1ebc45d3382244d73f1952
robmatania <rmatania@gmail.com>
2021-05-17 22:34:33 +0200

    PushBullet Notifications added

--- Mailbox Alarm/src/main.cpp
+++ Mailbox Alarm/src/main.cpp
@@ -1,10 +1,8 @@
 #include <Arduino.h>
 #include <WiFiClientSecure.h>
 
-/*
-  Example from WiFi > WiFiScan
-  Complete details at https://RandomNerdTutorials.com/esp32-useful-wi-fi-functions-arduino/
-*/
+#define PRINTF_BUF 50
+#define StartWordCount 32
 
 
 const char* host = "api.pushbullet.com";
@@ -13,6 +11,8 @@ const char* accessToken = "o.DCVgtxouN2DoW0D3jtOjEYSvZzX6KaI1";
 
 const char* ssid     = "Cloyes_Home";
 const char* password = "5A69A4FF96D69736CEADF539A6";
+
+void pushNotification(const char *MessageBody, const char *MessageTitle);
 
 void setup() {
   Serial.begin(9600);
@@ -44,7 +44,7 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-
+/*
   // Use WiFiClientSecure class to create TLS connection
   WiFiClientSecure client;
   Serial.print("connecting to ");
@@ -55,36 +55,52 @@ void setup() {
   }
   Serial.print("Connected to host: ");
   Serial.println(host);
+*/
+
+ // PushStatus("Mail Notificatio","You  have mail", "note"); 
+ pushNotification("You have MAIL", " ");
 }
 
 void loop() {
 
-/**************
-  Serial.println("scan start");
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-  if (n == 0) {
-      Serial.println("no networks found");
-  } else {
-    Serial.print(n);
-    Serial.println(" networks found");
-    for (int i = 0; i < n; ++i) {
-      // Print SSID and RSSI for each network found
-      Serial.print(i + 1);
-      Serial.print(": ");
-      Serial.print(WiFi.SSID(i));
-      Serial.print(" (");
-      Serial.print(WiFi.RSSI(i));
-      Serial.print(")");
-      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-      delay(10);
-    }
+
+} // Loop
+
+
+void pushNotification( const char *MessageBody, const char *MessageTitle) {
+
+String xx = "toto" + String(MessageBody);
+
+// Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
   }
-  Serial.println("");
 
-  // Wait a bit before scanning again
-  delay(5000);
+  String url = "/v2/pushes";
+  String messagebody = "{\"type\": \"note\", \"title\": \""+ String(MessageTitle)+"\", \"body\": \""+String(MessageBody)+"\"}\r\n";
+ // String messagebody = "{\"type\": \"note\", \"title\": \"ESP8266\", \"body\": \"Hello World!\"}\r\n";
+  Serial.print("requesting URL: ");
+  Serial.println(url);
 
-***************/
+
+  client.print(String("POST ") + url + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                 "Access-Token: " + accessToken + "\r\n" +
+                 "Content-Type: application/json\r\n" +
+                 "Content-Length: " +
+                 String(messagebody.length()) + "\r\n\r\n");
+
+  client.print(messagebody);
+
+  //print the response
+
+  while (client.available() == 0);
+
+  while (client.available()) {
+    String line = client.readStringUntil('\n');
+    Serial.println(line);
+  }
 }
