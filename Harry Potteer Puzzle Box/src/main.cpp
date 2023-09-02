a8f265d3c74a27c7b4b490d27698bc13c3cec176
robmatania <rmatania@gmail.com>
2023-09-02 10:23:55 +0200

    add electronic folder

--- Harry Potteer Puzzle Box/src/main.cpp
+++ Harry Potteer Puzzle Box/src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
 
-int hallSensorPin = A0;     
+//int hallSensorPin = A0;     
+int hallSensorPin = 2;     
+
 int ledPin =  13;    
 int state = 0; 
 long sensorValue;
+bool sVal;
 
 void setup() {
   Serial.begin(9600);
@@ -14,11 +17,21 @@ void setup() {
 
 void loop(){
   
+  /**
    sensorValue = analogRead(hallSensorPin);
+
    delay (1);
   //Serial.println(sensorValue);
 
   if (sensorValue > 600) {        
+    digitalWrite(ledPin, HIGH);  
+  } 
+  else {
+    digitalWrite(ledPin, LOW); 
+  }
+  **/
+ sVal = digitalRead(hallSensorPin);
+ if (sVal) {        
     digitalWrite(ledPin, HIGH);  
   } 
   else {
