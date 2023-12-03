f1dbd71c4422d64404625d454be2863e0890276b
robmatania <rmatania@gmail.com>
2023-12-03 23:52:27 +0100

    FSM structure

--- Harry Potteer Puzzle Box/src/main.cpp
+++ Harry Potteer Puzzle Box/src/main.cpp
@@ -1,61 +1,125 @@
+/*
+Harry Potter Puzzle Box
+
+Author: Robert Matania
+Date: 2023
+
+*/
 #include <Arduino.h>
+#include "EEPROM.h"
 
-//int hallSensorPin = A0;   Sensor PinPin
+#define DEBUG true
 
-int hallSensorPin = 2;     
-int fetOpPin = 3;
-int ledPin =  13;    
-int state = 0; 
-long sensorValue;
-bool sVal;
+#define VERSION 0.1
+#define STATE_ADDRESS 0
+
+
+enum puzzleStates {                         // Define enumerated type for state machine states
+  NONE,
+  IDLE, 
+  STATE_1, 
+  STATE_2, 
+  STATE_3,
+  STATE_4,
+  STATE_5,
+  STATE_6,
+  STATE_7
+};
+
+byte savedState;
+puzzleStates lastState, currentState;
+
+void idle() {
+// If entering the state, do initialization stuff
+  if (currentState != lastState) {         
+    lastState = currentState;
+   
+  }
+
+// Perform state tasks
+
+// Check for state transitions
+
+// If leaving the state, do clean up stuff
+ if (currentState != lastState) {         // If we are leaving the state, do clean up stuff
+    
+  }
+
+}
+
+
+void state_1() {
+
+}
+
+void state_2() {
+
+}
+
+void state_3() {
+
+}
+
+void state_4() {
+
+}
+
+void state_5() {
+
+}
+
+void state_6() {
+
+}
+
+
+
+
 
 void setup() {
+  
   Serial.begin(9600);
   
-  pinMode(ledPin, OUTPUT);      
-  pinMode(hallSensorPin, INPUT_PULLUP);     
-  pinMode(fetOpPin,OUTPUT);
+  savedState = EEPROM.read(STATE_ADDRESS);
 
-for (int i=0; i<3; i++)
-  {
-  digitalWrite(fetOpPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(fetOpPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-  }
+  if (savedState == 0xFF)
+    savedState = IDLE;
+  currentState = puzzleStates(savedState);
+  lastState = NONE;
+
+  #ifdef DEBUG
+   Serial.print(F("Version: "));
+   Serial.println(VERSION);
+  #endif
+
+  
+  
 }
 
 void loop(){
+  switch (currentState) {
+    
+    case NONE:
+      break;
+
+    case IDLE:
+      idle();
+      break;
+
+    case STATE_1:
+      state_1();
+      break;
   
-  /**
-   sensorValue = analogRead(hallSensorPin);
-
-   delay (1);
-  //Serial.println(sensorValue);
-
-  if (sensorValue > 600) {        
-    digitalWrite(ledPin, HIGH);  
-  } 
-  else {
-    digitalWrite(ledPin, LOW); 
-  }
-  **/
- 
- sVal = digitalRead(hallSensorPin);
- if (sVal) {        
-    digitalWrite(ledPin, HIGH);  
-    digitalWrite(fetOpPin, LOW); 
-  } 
-  else {
-    digitalWrite(ledPin, LOW); 
-    digitalWrite(fetOpPin, HIGH); 
+    case STATE_2:
+      state_2();
+      break;
+  
+    case STATE_3:
+      state_3();
+      break;
+  
+    default:
+      break;
   }
  
-
-/*
- digitalWrite(fetOpPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(fetOpPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
-  */
 }
