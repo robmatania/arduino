af88c37736a30a723feba460322f56a22996d8fd
robmatania <rmatania@gmail.com>
2023-12-09 22:50:55 +0100

    added main include

---
+++ Harry Potteer Puzzle Box/include/HPPuzzleBox.h
@@ -0,0 +1,11 @@
+#define GEMLED_G 5
+#define GEMLED_R 4
+#define GEM_1 50
+#define GEM_2 51
+#define GEM_3 52
+#define GEM_4 53
+
+#define PUCK_1 A9
+#define PUCK_2 A10
+#define PUCK_3 A11
+#define PUCK_4 A12
