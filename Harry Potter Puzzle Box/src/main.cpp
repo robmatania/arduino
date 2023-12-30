4b544f56e5e5bc96ca2a60d202fbef5bd7fa7384
robmatania <rmatania@gmail.com>
2023-12-30 17:45:54 +0100

    Puck states

--- Harry Potter Puzzle Box/src/main.cpp
+++ Harry Potter Puzzle Box/src/main.cpp
@@ -14,8 +14,9 @@ Date: 2023
 #include <Ticker.h>
 #include <HPPuzzleBox.h>
 
-#define DEBUG true
-
+#define NODEBUG 
+#define DEFAULT_VOLUME MIN_VOLUME
+#define SOUND_ONOFF  1 // 0 = off, 1 = on
 #define VERSION 0.1
 #define EPROM_STATE_ADDRESS 0
 
@@ -37,8 +38,32 @@ byte savedState;
 puzzleStates lastState, currentState;
 int mp3Busy = 1; // 0=playing; 1=not playing, 
 int helpBtn;
+byte puck_1_State = 0;
+byte puck_2_State = 0;
+byte puck_3_State = 0;
+byte puck_4_State = 0;
+
+byte newPuckStates = 0;
+byte lastPuckStates = 0;
+
 
 DY::Player player(&Serial2);
+
+//--------------------------------------------------------------------------------------
+byte readPuckStates(void){
+  byte puckState,tmpPuckState;
+
+  tmpPuckState = digitalRead(PUCK_1);
+  puckState = tmpPuckState;
+  tmpPuckState = digitalRead(PUCK_2);
+  puckState = puckState | tmpPuckState << 1;
+  tmpPuckState = digitalRead(PUCK_3);
+  puckState = puckState | tmpPuckState << 2;
+  tmpPuckState = digitalRead(PUCK_4);
+  puckState = puckState | tmpPuckState << 3;
+
+  return puckState;
+  }
 
 //--------------------------------------------------------------------------------------
 int readMp3State(void){
@@ -53,9 +78,11 @@ void stopMp3Play(void){
 // -------------------------------------------------------------------------------------
 void startMp3Play(int index,int vol)
 {
-  player.setVolume(vol); 
-  player.playSpecified(index);
-  Serial.println("Start Playing");
+  if (SOUND_ONOFF == 1){
+    player.setVolume(vol); 
+    player.playSpecified(index);
+   // Serial.println("Start Playing");
+  }
 }
 // -------------------------------------------------------------------------------------
 
@@ -115,14 +142,27 @@ void state_0() {
     lastState = currentState;
 
    
-    startMp3Play(1,30);
+    startMp3Play(2,DEFAULT_VOLUME);
   }
 
 // Perform state tasks
 // Check for state transitions
     mp3Busy = readMp3State();
-    Serial.print(F("mp3 State: "));
-    Serial.println(mp3Busy);
+#ifdef DEBUG
+Serial.print(F("mp3 State: "));
+Serial.println(mp3Busy);
+#endif
+  newPuckStates = readPuckStates();
+  if (newPuckStates < lastPuckStates){
+    Serial.print(lastPuckStates);  
+    Serial.print(" ");  
+    Serial.println(newPuckStates);  
+    startMp3Play(2,DEFAULT_VOLUME);
+  }
+  lastPuckStates = newPuckStates;
+  
+//Serial.print(F("Puck States: "));
+
 
 
 // If leaving the state, do clean up stuff
@@ -244,7 +284,10 @@ void setup() {
 
   pinMode(MP3_BUSY, INPUT_PULLUP);
   pinMode(HELP_BTN,INPUT_PULLUP);
-    
+  pinMode(PUCK_1,INPUT_PULLUP);  
+  pinMode(PUCK_2,INPUT_PULLUP);  
+  pinMode(PUCK_3,INPUT_PULLUP);  
+  pinMode(PUCK_4,INPUT_PULLUP);  
 
   currentState = INIT;
   lastState = NONE;
