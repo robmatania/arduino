f88de2e3a630146709811fd81b198b68f7237df0
robmatania <rmatania@gmail.com>
2023-12-30 09:28:00 +0100

    added include

---
+++ PuzzleBox/include/HPPuzzleBox.h
@@ -0,0 +1,42 @@
+#define GEMLED_G 5
+#define GEMLED_R 4
+#define GEM_1 50
+#define GEM_2 51
+#define GEM_3 52
+#define GEM_4 53
+
+#define PUCK_1 A9
+#define PUCK_2 A10
+#define PUCK_3 A11
+#define PUCK_4 A12
+
+#define HELP_LED 8
+#define HELP_BTN 10
+#define MP3_BUSY 11
+
+#define LATCH_2 32
+#define LATCH_3 33
+#define LATCH_4 34
+#define LATCH_5 35
+
+#define TRAP_2 36
+#define TRAP_3 37
+#define TRAP_4 38
+#define TRAP_5 39
+
+#define SPELL_LED_1 A6
+#define SPELL_LED_2 46
+#define SPELL_LED_3 44
+#define SPELL_LED_4 43
+#define SPELL_LED_5 42
+#define SPELL_LED_6 48
+#define SPELL_LED_7 49
+#define SPELL_LED_8 A7
+#define SPELL_LED_9 45
+#define SPELL_LED_10 47
+
+int spellLeds[10] = {SPELL_LED_1 ,SPELL_LED_2,SPELL_LED_3,SPELL_LED_4,SPELL_LED_5,SPELL_LED_6,SPELL_LED_7,SPELL_LED_8,SPELL_LED_9,SPELL_LED_10};
+
+#define DRAGON_TUBE A4
+#define DRAGON_SPELL_LED A5
+#define LID_CONTACT 12
